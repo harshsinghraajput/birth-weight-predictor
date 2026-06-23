2cfa451eb48813fc67530f0eb6b919d26529e9a8
Harsh Singh Rajput <iamharshsinghrajput@gmail.com>
2026-06-24 00:01:38 +0530

    my second commit

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def prediction():
 
         baby_df = pd.DataFrame(baby_data_cleaned)   
 
-        with open('model/model.pkl', 'rb') as obj:
+        with open('model.pkl', 'rb') as obj:
             model = pickle.load(obj)
 
         prediction = model.predict(baby_df)
